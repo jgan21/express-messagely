55d4d09dbd67533536f6da94d5c1c1023eebd821
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-08 19:20:03 -0600

    Added to route POST /login

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,29 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { UnauthorizedError, BadRequestError } = require("../expressError");
+const db = require("../db");
+const { authenticateJWT, ensureLoggedIn } = require("../middleware/auth");
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+
 
 /** POST /login: {username, password} => {token} */
+
+router.post("/login", async function(req, res, next){
+  if (req.body === undefined) throw new BadRequestError();
+
+  const { username, password } = req.body;
+
+  if (await User.authenticate(username, password)){
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  } else {
+    throw new UnauthorizedError("Invalid username/password");
+  }
+});
 
 
 /** POST /register: registers, logs in, and returns token.
