5be80ef33eba180503fa48dceaa225ea7a151353
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-09 13:35:13 -0600

    added updateLoginTimeStamp to post login route

--- routes/auth.js
+++ routes/auth.js
@@ -2,39 +2,12 @@
 
 const Router = require("express").Router;
 const router = new Router();
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user")
 
 const { UnauthorizedError, BadRequestError } = require("../expressError");
-const db = require("../db");
-const { authenticateJWT, ensureLoggedIn } = require("../middleware/auth");
-const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
-
-
-/** POST /register: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- */
-
-router.post("/register", async function(req, res, next) {
-
-  if (req.body === undefined) throw new BadRequestError();
-
-  // Register new user
-  const newUserInfo = await User.register(req.body);
-
-  // Get token for newly registered user
-  const token = jwt.sign({ newUserInfo }, SECRET_KEY);
-
-  // Send newUserInfo to login route, which returns token
-  next({ token });
-  // notes: await? next? what is the best method
-
-});
-
-
+const { SECRET_KEY } = require("../config");
 
 /** POST /login: {username, password} => {token} */
 
@@ -58,8 +31,27 @@ router.post("/login", async function(req, res, next){
   }
 });
 
+/** POST /register: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ */
 
+router.post("/register", async function(req, res, next) {
 
+  if (req.body === undefined) throw new BadRequestError();
 
+  // Register new user
+  const newUserInfo = await User.register(req.body);
+
+  // Get token for newly registered user
+  const token = jwt.sign({ newUserInfo }, SECRET_KEY);
+
+  // Update most recent time logged in to current_timestamp
+  await User.updateLoginTimestamp(username);
+
+  // Send newUserInfo to login route, which returns token
+  return res.json({ token });
+
+});
 
 module.exports = router;
